06d45d625ebfa4dc493d01248a4ebfb5ceff1645
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-05-18 18:08:49 +0100

    Setting up layout page

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import "@styles/globals.css"
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts"
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
