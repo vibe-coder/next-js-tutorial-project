6932f97415d5f3a64901ba8024aa02dfdcd0140b
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-05-20 14:38:50 +0100

    Setting up log in authentication

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,29 @@
+
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+
+
+
+
+
+
+const handler = NextAuth({
+  providers:[
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+    })
+  ],
+  
+  async session({session}){
+
+  },
+
+  async signIn({profile}){
+
+  }
+})
+
+export {handler as GET, handler as POST, }
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
 import "@styles/globals.css"
 import Nav from "@components/Nav"
-
-
 import Provider from "@components/Provider"
 
 export const metadata = {
@@ -13,17 +11,18 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
       <body>
-
-        {/* Gradient background for Layout page */}
-        <div className="main">
-          <div className="gradient" />
-        </div>
-
-        {/*Main part of our application */}
-        <main className="app">
-          <Nav/>
-          {children}
-        </main>
+        <Provider>
+          {/* Gradient background for Layout page */}
+          <div className="main">
+            <div className="gradient" />
+          </div>
+    
+          {/*Main part of our application */}
+          <main className="app">
+            <Nav/>
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
